a5ab7fdb8e88c53213408ffedb8cc715abcd344c
Elad Moshe <pheromem@gmail.com>
2024-08-19 01:34:50 +0300

    Specified names for the first and last name columns

--- app/src/main/java/com/pherom/easysaleassignment/model/User.java
+++ app/src/main/java/com/pherom/easysaleassignment/model/User.java
@@ -1,5 +1,6 @@
 package com.pherom.easysaleassignment.model;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
@@ -9,7 +10,9 @@ public class User {
     @PrimaryKey(autoGenerate = true)
     private int id;
     private String email;
+    @ColumnInfo(name = "first_name")
     private String firstName;
+    @ColumnInfo(name = "last_name")
     private String lastName;
     private String avatar;
 
