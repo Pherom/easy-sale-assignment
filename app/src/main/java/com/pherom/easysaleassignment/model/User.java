b2c5d36be594e5aae9e7f52e47adbe327db364f9
Elad Moshe <pheromem@gmail.com>
2024-08-19 00:26:16 +0300

    Created a User entity class

---
+++ app/src/main/java/com/pherom/easysaleassignment/model/User.java
@@ -0,0 +1,51 @@
+package com.pherom.easysaleassignment.model;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "users")
+public class User {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String avatar;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
