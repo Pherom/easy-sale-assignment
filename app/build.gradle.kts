1023df12a2025439eff9d617f1613e71f880f5e5
Elad Moshe <pheromem@gmail.com>
2024-08-19 18:04:37 +0300

    Added dependency for room-testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.room.runtime)
     implementation(libs.room.compiler)
+    implementation(libs.room.testing)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
