e6a5d7449945e0edf0c94c1316dffe34d422e7c8
Elad Moshe <pheromem@gmail.com>
2024-08-21 13:37:10 +0300

    Created UserDto and UserResponse classes

---
+++ app/src/main/java/com/pherom/easysaleassignment/network/UserDto.java
@@ -0,0 +1,69 @@
+package com.pherom.easysaleassignment.network;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Objects;
+
+public class UserDto {
+
+    private int id;
+    private String email;
+    @SerializedName(name = "first_name")
+    private String firstName;
+    @SerializedName(name = "last_name")
+    private String lastName;
+    private String avatar;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDto userDto = (UserDto) o;
+        return id == userDto.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ app/src/main/java/com/pherom/easysaleassignment/network/UserResponse.java
@@ -0,0 +1,70 @@
+package com.pherom.easysaleassignment.network;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+import java.util.Objects;
+
+public class UserResponse {
+
+    private int page;
+    @SerializedName("per_page")
+    private int perPage;
+    private int total;
+    @SerializedName("total_pages")
+    private int totalPages;
+    List<UserDto> data;
+
+    public int getPage() {
+        return page;
+    }
+
+    public int getPerPage() {
+        return perPage;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public List<UserDto> getData() {
+        return data;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public void setPerPage(int perPage) {
+        this.perPage = perPage;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public void setData(List<UserDto> data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserResponse that = (UserResponse) o;
+        return page == that.page;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(page);
+    }
+}
